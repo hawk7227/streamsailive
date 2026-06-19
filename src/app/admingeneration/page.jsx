3b90c349f147d5ed8db90218bd4a272187486b1e
hawk7227 <hawkinsmarcus127@gmail.com>
2026-06-18 22:48:15 -0700

    Mount safe reference uploader on admingeneration

--- src/app/admingeneration/page.jsx
+++ src/app/admingeneration/page.jsx
@@ -4,6 +4,7 @@ import OriginalAdmingenerationPage from "./OriginalAdmingenerationPage";
 import CompactAnalyzerVideoMode from "@/components/admingeneration/CompactAnalyzerVideoMode";
 import AnalyzerPreviewIntelligenceDock from "@/components/admingeneration/AnalyzerPreviewIntelligenceDock";
 import LiveVoiceConversationPanel from "@/components/admingeneration/LiveVoiceConversationPanel";
+import SafeReferenceUploadOverlay from "@/components/admingeneration/SafeReferenceUploadOverlay";
 
 export default function AdminGenerationPage() {
   return (
@@ -12,6 +13,7 @@ export default function AdminGenerationPage() {
       <AnalyzerPreviewIntelligenceDock />
       <CompactAnalyzerVideoMode />
       <LiveVoiceConversationPanel />
+      <SafeReferenceUploadOverlay />
     </>
   );
 }
