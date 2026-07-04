3dc280fa314350c20088dcc9cb1bb5f4de362116
hawk7227 <hawkinsmarcus127@gmail.com>
2026-07-03 23:36:35 -0700

    Remove embedded-era bridges from Streams AI page

--- src/app/streams-ai/page.tsx
+++ src/app/streams-ai/page.tsx
@@ -1,21 +1,7 @@
 import StreamsClientShell from "@/components/streams-ai/current-chat/StreamsClientShell";
-import ConversationStateEmitter from "@/components/streams-ai/current-chat/ConversationStateEmitter";
-import StreamsAIBuilderModeBridge from "./StreamsAIBuilderModeBridge";
-import StreamsAIBuilderProofBridge from "./StreamsAIBuilderProofBridge";
-import StreamsAIPageVisualFix from "./StreamsAIPageVisualFix";
-import StreamsAIChatBehaviorTuning from "./StreamsAIChatBehaviorTuning";
 import mobileChatStyles from "./StreamsAIMobileChat.module.css";
 
 export default function StreamsAIPage() {
   void mobileChatStyles;
-  return (
-    <>
-      <StreamsAIBuilderModeBridge />
-      <StreamsAIBuilderProofBridge />
-      <StreamsAIPageVisualFix />
-      <StreamsAIChatBehaviorTuning />
-      <ConversationStateEmitter />
-      <StreamsClientShell />
-    </>
-  );
+  return <StreamsClientShell />;
 }
