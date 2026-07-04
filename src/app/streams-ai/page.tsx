42cf0d142f1c01e0b41c6fd969a0f8e0348429e2
hawk7227 <hawkinsmarcus127@gmail.com>
2026-07-04 10:23:43 -0700

    Mount Streams AI client fast reply bridge

--- src/app/streams-ai/page.tsx
+++ src/app/streams-ai/page.tsx
@@ -1,4 +1,5 @@
 import StreamsClientShell from "@/components/streams-ai/current-chat/StreamsClientShell";
+import StreamsAIFastReplyBridge from "./StreamsAIFastReplyBridge";
 import StreamsAIMobileKeyboardBridge from "./StreamsAIMobileKeyboardBridge";
 import mobileChatStyles from "./StreamsAIMobileChat.module.css";
 import mobileKeyboardStyles from "./StreamsAIMobileKeyboard.module.css";
@@ -8,6 +9,7 @@ export default function StreamsAIPage() {
   void mobileKeyboardStyles;
   return (
     <>
+      <StreamsAIFastReplyBridge />
       <StreamsAIMobileKeyboardBridge />
       <StreamsClientShell />
     </>
