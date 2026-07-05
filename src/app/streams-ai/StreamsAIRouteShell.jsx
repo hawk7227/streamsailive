508771515b7e97d1debec9ccc5315663796a64cd
hawk7227 <hawkinsmarcus127@gmail.com>
2026-07-05 02:35:20 -0700

    Add shared Streams AI route shell

---
+++ src/app/streams-ai/StreamsAIRouteShell.jsx
@@ -0,0 +1,22 @@
+import StreamsClientShell from "@/components/streams-ai/current-chat/StreamsClientShell";
+import StreamsAIDesktopVisualBridge from "./StreamsAIDesktopVisualBridge";
+import StreamsAIEmptyComposerPositionBridge from "./StreamsAIEmptyComposerPositionBridge";
+import StreamsAIMobileKeyboardBridge from "./StreamsAIMobileKeyboardBridge";
+import desktopConsoleStyles from "./StreamsAIDesktopConsole.module.css";
+import mobileChatStyles from "./StreamsAIMobileChat.module.css";
+import mobileKeyboardStyles from "./StreamsAIMobileKeyboard.module.css";
+
+export default function StreamsAIRouteShell() {
+  void desktopConsoleStyles;
+  void mobileChatStyles;
+  void mobileKeyboardStyles;
+
+  return (
+    <>
+      <StreamsAIDesktopVisualBridge />
+      <StreamsAIEmptyComposerPositionBridge />
+      <StreamsAIMobileKeyboardBridge />
+      <StreamsClientShell />
+    </>
+  );
+}
