bf80004250fa92bf6480b989adc202b139ad6639
hawk7227 <hawkinsmarcus127@gmail.com>
2026-07-06 12:07:57 -0700

    Use Streams AI layout bridges on session route

--- src/app/streams-ai/[sessionId]/page.tsx
+++ src/app/streams-ai/[sessionId]/page.tsx
@@ -1,5 +1,21 @@
 import StreamsClientShell from "@/components/streams-ai/current-chat/StreamsClientShell";
+import StreamsAIDesktopVisualBridge from "../StreamsAIDesktopVisualBridge";
+import StreamsAIEmptyComposerPositionBridge from "../StreamsAIEmptyComposerPositionBridge";
+import StreamsAIMobileKeyboardBridge from "../StreamsAIMobileKeyboardBridge";
+import desktopConsoleStyles from "../StreamsAIDesktopConsole.module.css";
+import mobileChatStyles from "../StreamsAIMobileChat.module.css";
+import mobileKeyboardStyles from "../StreamsAIMobileKeyboard.module.css";
 
 export default function StreamsAISessionPage() {
-  return <StreamsClientShell />;
+  void desktopConsoleStyles;
+  void mobileChatStyles;
+  void mobileKeyboardStyles;
+  return (
+    <>
+      <StreamsAIDesktopVisualBridge />
+      <StreamsAIEmptyComposerPositionBridge />
+      <StreamsAIMobileKeyboardBridge />
+      <StreamsClientShell />
+    </>
+  );
 }
