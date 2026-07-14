de84b24927421f710fb852ebdc5e34bc081a98a7
hawk7227 <hawkinsmarcus127@gmail.com>
2026-07-13 23:04:31 -0700

    Remove legacy Streams desktop bridge from session route

--- src/app/streams-ai/[sessionId]/page.tsx
+++ src/app/streams-ai/[sessionId]/page.tsx
@@ -1,19 +1,13 @@
 import StreamsClientShell from "@/components/streams-ai/current-chat/StreamsClientShell";
-import StreamsAIDesktopVisualBridge from "../StreamsAIDesktopVisualBridge";
-import StreamsAIEmptyComposerPositionBridge from "../StreamsAIEmptyComposerPositionBridge";
 import StreamsAIMobileKeyboardBridge from "../StreamsAIMobileKeyboardBridge";
-import desktopConsoleStyles from "../StreamsAIDesktopConsole.module.css";
 import mobileChatStyles from "../StreamsAIMobileChat.module.css";
 import mobileKeyboardStyles from "../StreamsAIMobileKeyboard.module.css";
 
 export default function StreamsAISessionPage() {
-  void desktopConsoleStyles;
   void mobileChatStyles;
   void mobileKeyboardStyles;
   return (
     <>
-      <StreamsAIDesktopVisualBridge />
-      <StreamsAIEmptyComposerPositionBridge />
       <StreamsAIMobileKeyboardBridge />
       <StreamsClientShell />
     </>
