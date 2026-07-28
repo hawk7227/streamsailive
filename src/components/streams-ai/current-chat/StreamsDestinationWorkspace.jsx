5e31a0540472471477f346090a2b91dc37005302
hawk7227 <hawkinsmarcus127@gmail.com>
2026-07-28 05:08:39 -0700

    Route Website Project to standalone launch workspace

--- src/components/streams-ai/current-chat/StreamsDestinationWorkspace.jsx
+++ src/components/streams-ai/current-chat/StreamsDestinationWorkspace.jsx
@@ -1,5 +1,14 @@
 "use client";
 
 import StreamsUniversalProjectWorkspace from "./StreamsUniversalProjectWorkspace";
+import WebsiteLaunchWorkspace from "./WebsiteLaunchWorkspace";
 
-export default StreamsUniversalProjectWorkspace;
+export default function StreamsDestinationWorkspace(props) {
+  if (props?.destination === "website-builder") {
+    return <><WebsiteLaunchWorkspace /><style jsx global>{`
+      .withNewChatVisualSample>.newChatNavigationVisualSample{display:none!important}
+      .withNewChatVisualSample>.streamsDestinationFrame{margin-left:0!important;width:100%!important}
+    `}</style></>;
+  }
+  return <StreamsUniversalProjectWorkspace {...props} />;
+}
