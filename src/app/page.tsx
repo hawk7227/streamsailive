0bf5bb66140dea26a13837530dc2880fca6f8dbd
hawk7227 <hawkinsmarcus127@gmail.com>
2026-08-05 09:12:24 -0700

    Mount landing brain runtime bridge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 
 import Navbar from "@/components/layout/Navbar";
 import StreamsWorkspaceLanding from "@/components/home/StreamsWorkspaceLanding";
+import LandingBrainRuntimeBridge from "@/components/home/LandingBrainRuntimeBridge";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <LandingBrainRuntimeBridge />
       <StreamsWorkspaceLanding />
     </>
   );
